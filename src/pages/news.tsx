a7d99f9baa36a5ac533f3d5c0cc434c8afe70c6d
João Pedro Chaves <joaopedroaschaves@gmail.com>
2024-05-29 02:06:17 -0300

    feat: implementacao da funcionalidade de deletar noticia

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -1,15 +1,17 @@
-import NavigationCard from "../components/NavigationCard";
 import Layout from "../components/Layout";
 import { Modal } from "../components/Modal";
 import CreateNewsForm from "../components/CreateNewsForm";
 import { useEffect, useState } from "react";
-import { Flex, Table, TableContainer, Tbody, Td, Thead, Tr, useDisclosure } from "@chakra-ui/react";
+import { Flex, Table, TableContainer, Tbody, Td, Thead, Tr, useDisclosure, Text, Icon } from "@chakra-ui/react";
 import { BiEditAlt } from "react-icons/bi";
 import { INews } from "../services/models/news";
 import newsService from "../services/news";
 import { AxiosResponse } from "axios";
 import { toast } from "../components/Toast";
 import UpdateNewsForm from "../components/UpdateNewsForm";
+import { MdDeleteForever } from "react-icons/md";
+import DeleteNewsForm from "../components/DeleteNewsForm";
+import { IoAdd } from "react-icons/io5";
 
 const News = () => {
   const [refreshRequest, setRefreshRequest] = useState<boolean>(false);
@@ -27,9 +29,20 @@ const News = () => {
     onOpen: onOpenUpdateNewsModal,
   } = useDisclosure();
 
+  const {
+    isOpen: isOpenDeleteNewsModal,
+    onClose: onCloseDeleteNewsModal,
+    onOpen: onOpenDeleteNewsModal,
+  } = useDisclosure();
+
   const handleUpdate = (news: INews) => {
     if (news) setSelectedNews(news);
     onOpenUpdateNewsModal();
+  };
+
+  const handleDelete = (news: INews) => {
+    if (news) setSelectedNews(news);
+    onOpenDeleteNewsModal();
   };
 
   const fetchItems = async () => {
@@ -48,12 +61,22 @@ const News = () => {
 
   return (
     <Layout direction="column">
+      <Text fontSize="7xl" textAlign="center" color="white">Notícias</Text>
       <Flex justifyContent="right">
-        <NavigationCard 
-          title="Criar"
-          description="Criar notícia"
+        <Flex
           onClick={() => onOpenCreateNewsModal()}
-        />
+          bgColor="whiteAlpha.500"
+          marginBottom="20px"
+          borderRadius="10px"
+          cursor="pointer"
+          gap="20px"
+          alignItems="center"
+          padding="10px"
+          _hover={{ transform: 'scale(1.02)'}}
+        >
+          <Text fontSize="xl">Criar Notícia</Text>
+          <Icon as={IoAdd} boxSize={10}/>
+        </Flex>
       </Flex>
       <TableContainer
         borderRadius="15px"
@@ -73,6 +96,7 @@ const News = () => {
               <Td>Título</Td>
               <Td>Descrição</Td>
               <Td />
+              <Td />
             </Tr>
           </Thead>
           <Tbody fontWeight="semibold" maxHeight="200px">
@@ -91,6 +115,16 @@ const News = () => {
                 >
                   <button>
                     <BiEditAlt size={23} />
+                  </button>
+                </Td>
+                <Td
+                  onClick={
+                    () => handleDelete(news)
+                  }
+                  width="5%"
+                >
+                  <button>
+                    <MdDeleteForever size={23} />
                   </button>
                 </Td>
               </Tr>
@@ -133,6 +167,27 @@ const News = () => {
         >
           <UpdateNewsForm
             onClose={onCloseUpdateNewsModal}
+            refreshRequest={refreshRequest}
+            setRefreshRequest={setRefreshRequest}
+            selectedNews={selectedNews}
+          />
+        </Flex>
+      </Modal>
+      <Modal
+        title={`Remover Notícia`}
+        isOpen={isOpenDeleteNewsModal}
+        onClose={onCloseDeleteNewsModal}
+        size="4xl"
+      >
+        <Flex
+          height="100%"
+          alignItems="center"
+          justifyContent="center"
+          flexDirection="column"
+          gap="16px"
+        >
+          <DeleteNewsForm
+            onClose={onCloseDeleteNewsModal}
             refreshRequest={refreshRequest}
             setRefreshRequest={setRefreshRequest}
             selectedNews={selectedNews}
